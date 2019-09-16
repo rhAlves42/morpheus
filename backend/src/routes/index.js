4c2094447d72fd3630bf25143a208497708aebfd
Ricardo Henrique <ricardo@wcore.com.br>
2019-09-16 17:18:24 -0300

    requisição cors origin resolvida, api ainda nao salva no arquivo txt, porem retorna 200

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
-router.get('/names', (req, res, next) => {
+router.get('/names/', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   const file = fs.readFileSync('nomes.txt', 'utf8')
   const names = file.split('\n')
   res.status(200).json(names)
 })
-router.post('/names', (req, res, next) => {
+router.post('/names/', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   const file = fs.readFileSync('nomes.txt', 'utf8')
   let names = file.split('\n')
@@ -16,7 +16,7 @@ router.post('/names', (req, res, next) => {
   if (name == '') {
     res.status(400).send({message: "Campo nome não pode estar vazio"})
   }
-  names.concat(names)
-  res.status(200).send({message: "Nome cadastrado com sucesso!"})
+  names.concat(name)
+  res.status(200).send({message: "Nome cadastrado com sucesso!" + req.body.name})
 })
 module.exports = router
