7c0d1836112a3da9b45bb927b0088b87a08479bd
Ricardo Henrique <ricardo@wcore.com.br>
2019-09-17 20:42:58 -0300

    corrigindo cors

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import axios from 'axios'
 import { Form, Input, Button } from 'antd';
-import NamesListView from '../containers/NamesListView'
 
 class CustomForm extends React.Component {
   state = {
